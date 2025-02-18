c1545e55c46844acfcd3d9b12225e8d6dc4406b3
furahaderick <yvesderricks@gmail.com>
2025-02-18 23:00:54 +0200

    Turning problems into code

--- src/example/tip_calculator.py
+++ src/example/tip_calculator.py
@@ -1,1 +1,14 @@
-print("Hello, Python Apps")
+bill_amount = 0
+tip_rate = 0
+tip = 0
+total_amount = 0
+
+bill_amount = float(input("Enter bill amount: $ "))
+tip_rate = float(input("Enter tip percentage: "))
+
+tip = round(bill_amount * (tip_rate / 100), 2)
+
+total_amount = bill_amount + tip
+
+print(f"Tip: ${tip}")
+print(f"Total: ${total_amount}")
