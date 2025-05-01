a7a4b988b1f73666534c4541bdefe5de8bfe8167
furahaderick <yvesderricks@gmail.com>
2025-05-01 08:34:12 +0200

    Legal Driving Age - challenge

--- src/decisions/3.legal_driver.py
+++ src/decisions/3.legal_driver.py
@@ -11,17 +11,77 @@
 from enum import Enum
 
 
+class NegativeAgeInput(Exception):
+    pass
+
+
 class Constants(Enum):
     ACCEPTED_AGE = 16
 
 
 age_input = 0
 
+# try:
+#     age_input = int(input("What is your age? "))
+#     if age_input < 0:
+#         raise NegativeAgeInput()
+# except ValueError:
+#     print("Invalid input received")
+# except NegativeAgeInput:
+#     print("You can't use negative numbers")
+# else:
+#     print(
+#         f"{'You are not old enough to drive' if age_input < Constants.ACCEPTED_AGE.value else 'You are old enough to drive'}"
+#     )
+
+
+# Challenge:
+# Instead of hard-coding the driving age in your program
+# logic, research driving ages for various countries and
+# create a lookup table for the driving ages and countries.
+# Prompt for the age, and display which countries the
+# user can legally drive in.
+
+COUNTRIES_DRIVING_AGES = [
+    {"country_name": "Rwanda", "accepted_age": 18},
+    {"country_name": "Tanzania", "accepted_age": 18},
+    {"country_name": "Morocco", "accepted_age": 18},
+    {"country_name": "USA", "accepted_age": 16},
+    {"country_name": "France", "accepted_age": 17},
+    {"country_name": "Cameroon", "accepted_age": 16},
+    {"country_name": "Uganda", "accepted_age": 18},
+]
+
+accepted_countries = []
+unaccepted_countries = []
+
 try:
-    age_input = int(input("What is your age? "))
+    age_input = int(input("What is your age?: "))
+    if age_input < 0:
+        raise NegativeAgeInput()
+
 except ValueError:
-    print("Invalid input received")
+    print("Only numerals please")
+except NegativeAgeInput:
+    print("You can't use negative numbers")
 else:
-    print(
-        f"{'You are not old enough to drive' if age_input < Constants.ACCEPTED_AGE.value else 'You are old enough to drive'}"
-    )
+
+    for country in COUNTRIES_DRIVING_AGES:
+        if country["accepted_age"] > age_input:
+            unaccepted_countries.append(country["country_name"])
+        else:
+            accepted_countries.append(country["country_name"])
+
+    if len(accepted_countries) > 0:
+        print("You can drive in:")
+        for country in accepted_countries:
+            print(f"- {country}")
+    else:
+        print("You can't drive in any country as far as we know")
+
+    if len(unaccepted_countries) > 0:
+        print("You can't drive in:")
+        for country in unaccepted_countries:
+            print(f"- {country}")
+    else:
+        print("Yep, You can drive in all countries (as far as we know)")
